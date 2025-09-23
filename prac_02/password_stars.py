77fd483a27ef9c59ebda149b233c37bafd1ceb51
Kate Archer <kate.archer1@my.jcu.edu>
2025-09-23 13:30:36 +1000

    Refractor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,11 +1,20 @@
 def main():
     """ give a valid password """
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password: str):
+    print(len(password) * "*")
+
+
+def get_password() -> str:
     password = input("What is the password?: ")
     minimum_length = 4
     while len(password) < minimum_length:
         print("Password too short. (Min length of 4)")
         password = input("What is the password?: ")
-    print(len(password) * "*")
+    return password
 
 
 main()
