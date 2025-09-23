0f933e45159047672bfe1044f54fe2bfd3eb0f53
Kate Archer <kate.archer1@my.jcu.edu>
2025-09-23 13:26:23 +1000

    Add main() function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,11 @@
-password = input("What is the password?: ")
-MINIMUM_LENGTH = 4
-while len(password) < MINIMUM_LENGTH:
-    print("Password too short. (Min length of 4)")
+def main():
+    """ give a valid password """
     password = input("What is the password?: ")
-print(len(password) * "*")
+    minimum_length = 4
+    while len(password) < minimum_length:
+        print("Password too short. (Min length of 4)")
+        password = input("What is the password?: ")
+    print(len(password) * "*")
+
+
+main()
