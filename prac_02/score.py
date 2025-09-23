4028d5ac027ca9b45547b4bdce103efdc604acc0
Kate Archer <kate.archer1@my.jcu.edu>
2025-09-23 14:49:09 +1000

    Improve docstrings and spacing

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,17 +3,21 @@ CP1404/CP5632 - Practical 1
 Program to determine score status
 Fixed to determine score status, with function
 """
+
+# Note 50 is included in the pass boundary
+
 import random
 
 
 def main():
-    """ Get a numeric score and display its status """
+    """Get a numeric score and display its status."""
     # score = float(input("Enter score: "))
     score = random.randint(0, 100)
     print(determine_status(score))
 
 
 def determine_status(score: float):
+    """Determine the status of a given score."""
     if score < 0 or score > 100:
         print("Invalid score")
     elif score >= 90:
