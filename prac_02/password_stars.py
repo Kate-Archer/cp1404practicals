d421edb8ab199a2b614afa1e607766be6b25acdf
Kate Archer <kate.archer1@my.jcu.edu>
2025-09-23 15:12:14 +1000

    Improve password_stars coding for the functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,20 +1,44 @@
+"""
+CP1404 - Practical
+Get a password with minimum length and display asterisks
+"""
+# Define the minimum password length (constant)
+MINIMUM_LENGTH = 4
+
+
 def main():
-    """ Give a valid password """
-    password = get_password()
-    print_password(password)
+    """Get and print a valid password with functions."""
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisks(password)
 
 
-def print_password(password: str):
-    print(len(password) * "*")
 
-
-def get_password() -> str:
-    password = input("What is the password?: ")
-    minimum_length = 4
-    while len(password) < minimum_length:
-        print("Password too short. (Min length of 4)")
-        password = input("What is the password?: ")
+# Attempt 2:
+def get_password(MINIMUM_LENGTH):
+    """Get password, ensuring it meets the minimum_length requirement."""
+    password = input(f"Enter password (minimum length {MINIMUM_LENGTH} characters): ")
+    while len(password) < MINIMUM_LENGTH:
+        print(f"Password too short. (Minimum length of {MINIMUM_LENGTH}")
+        password = input(f"Enter password (minimum length {MINIMUM_LENGTH} characters): ")
     return password
 
 
+def print_asterisks(sequence):
+    """Print as many asterisks as there are characters in the passed-in sequence."""
+    print('*' * len(sequence))
+
+
 main()
+
+# Attempt 1:
+# def get_password() -> str:
+#     """Get password, ensuring it meets the minimum length requirement."""
+#     password = input("What is the password?: ")
+#     while len(password) < MINIMUM_LENGTH:
+#         print("Password too short. (Min length of 4)")
+#         password = input("What is the password?: ")
+#     return password
+#
+# def print_asterisks(password: str):
+#     """Print the same amount of asterisks as characters in the password."""
+#     print(len(password) * "*")
