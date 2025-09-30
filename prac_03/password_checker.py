dcee485642ac15f3c51d107f44116b405f4f6281
Kate Archer <kate.archer1@my.jcu.edu>
2025-09-30 14:00:40 +1000

    Clean up the formatting for the password_checker code and remove TODOs

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -5,7 +5,7 @@ Password checker "skeleton" code to help you get started
 
 MIN_LENGTH = 2
 MAX_LENGTH = 6
-IS_SPECIAL_CHARACTER_REQUIRED = False
+IS_SPECIAL_CHARACTER_REQUIRED = False  # Set to fault, it is not required
 SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
@@ -27,15 +27,18 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
+    # Not return a password if the password is below the minimum length or
+    # above the maximum length as it is invalid
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
 
+    # Set the characters to 0 initially
     number_of_digit = 0
     number_of_lower = 0
     number_of_upper = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
+        # Count the number of different character types in password
         if character.isdigit():
             number_of_digit += 1
         elif character.islower():
@@ -45,15 +48,15 @@ def is_valid_password(password):
         elif character in SPECIAL_CHARACTERS:
             number_of_special += 1
 
-    # TODO: if any of the 'normal' counts are zero, return False
+    # Return False if any of the 'normal' counts are zero
     if number_of_digit == 0 or number_of_lower == 0 or number_of_upper == 0:
         return False
-    # TODO: if special characters are required, then check the count of those
+    # Count the amount of special characters if the special characters required is set to True
     # and return False if it's zero
     if IS_SPECIAL_CHARACTER_REQUIRED and number_of_special == 0:
         return False
 
-        # if we get here (without returning False), then the password must be valid
+    # Return True if all other checks have not returned False (password must be valid)
     return True
 
 
