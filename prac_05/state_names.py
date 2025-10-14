f9b51cd4b69a6ee95cd2a50d3dd23c66964c82ef
Kate Archer <kate.archer1@my.jcu.edu>
2025-10-14 12:26:03 +1000

    Update state_names to print abbreviation is full name

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,12 +7,17 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+#print(CODE_TO_NAME)
+max_length_name = max(len(name) for name in CODE_TO_NAME)
+max_length_code = max(len(code) for code in CODE_TO_NAME)
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:<{max_length_code}} is {name:>{max_length_name}}")
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+
+    state_code = input("Enter short state: ").upper()
