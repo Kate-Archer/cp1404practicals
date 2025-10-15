ec224e76e9a6bc7156c0d215a7bfe068598ebd4d
Kate Archer <kate.archer1@my.jcu.edu>
2025-10-15 13:33:03 +1000

    Complete prac 5 word_occurences with the text input set to lower so that 'A' and 'a' will equal the same word

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -1,18 +1,18 @@
 """
 Word Occurrences
 Estimate: 20 minutes
-Actual:    minutes start 12:53
+Actual: 18 minutes
 """
 
+# Add dictionary for words to count (lowercase because it is not a global variable, it changes)
 word_to_count = {}
-text = input("Give a string of words:")
+# Split the text input, count the frequency of each word and print the count next to the word
+text = input("Please write a sentence of text:").lower()
 words = text.split()
 for word in words:
-    #print(len(word))
-    frequency = word_to_count.get(word, 0)
-    word_to_count[word] = frequency + 1
+    word_frequency = word_to_count.get(word, 0)
+    word_to_count[word] = word_frequency + 1
 
-max_length = max((len(word) for word in words))
+max_length_word = max((len(word) for word in words))
 for word in sorted(word_to_count):
-    print(max_length)
-    print(f"{word:{max_length}} : {word_to_count[word]}")
+    print(f"{word:{max_length_word}} : {word_to_count[word]}")
