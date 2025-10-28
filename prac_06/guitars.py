db16cc4f8acbea328b684bc2c081a056a2e9d151
Kate Archer <kate.archer1@my.jcu.edu>
2025-10-28 16:09:18 +1000

    Correctly format printing the index of each new guitar (from 1)

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,22 +9,28 @@ from prac_06.guitar import Guitar
 def main():
     """eee"""
     guitars = []
+
+    print("My guitars!")
     name = str(input("Name:"))
 
-
     while name != "":
-        year = int(input("Year:"))
-        cost = float(input("Cost:"))
-        print(f"{name} ({year}) : ${cost: .2f} added.")
-        new_guitar = Guitar(name, year, cost)
-        guitars.append(new_guitar)
+        try:
+            year = int(input("Year:"))
+            cost = float(input("Cost: $"))
+            new_guitar = Guitar(name, year, cost)
+            guitars.append(new_guitar)
+            print(f"{name} ({year}) : ${cost: .2f} added.")
+            name = str(input("Name:"))
+        except ValueError:
+            print("Year must be an integer (e.g. 2020). Cost must be a number.")
+    print("...snip...")
     print("These are my guitars:")
-    for index, (name, year, cost) in enumerate(guitars, start=1):
-        for guitar in guitars:
-            print(f"Guitar {index}: {guitar}")
+    for index, new_guitar in enumerate(guitars, start=1):
+        print(f"Guitar {index}: {new_guitar}")
 
-
-
+#
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
 
 main()
