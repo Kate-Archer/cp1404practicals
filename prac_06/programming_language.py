f8070af3b3987736dc27c389b516b191d5d4b677
Kate Archer <kate.archer1@my.jcu.edu>
2025-10-28 12:52:17 +1000

    Fix and simplify the is_dynamic method to return true if the typing is dynamic

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -10,18 +10,17 @@ class ProgrammingLanguage:
     def __init__(self, language = "", typing = "", reflection = "", year = 0):
         self.language = language.title()
         self.typing = typing.title()
-        self.reflection = reflection.title()
+        self.reflection = reflection
         self.year = year
 
 
     def is_dynamic(self):
-        if self.typing.lower() == "dynamic":
-            return True
-        return None
+        """Determine if langauge is dynamically typed"""
+        return self.typing == "Dynamic"
 
     def __str__(self):
         return (f"{self.language}, {self.typing} Typing, "
-                f"Reflection = {self.is_dynamic}, First appear in {self.year}")
+                f"Reflection = {self.reflection}, First appeared in {self.year}")
 
     def __repr__(self):
         return str(self)
