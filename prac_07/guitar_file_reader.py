bc74f4c88fca39f9ed846c4086f12284c06ba53b
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-04 14:09:45 +1000

    Update the guitar code to add a new guitar added by the user

--- prac_07/guitar_file_reader.py
+++ prac_07/guitar_file_reader.py
@@ -3,6 +3,7 @@ Prac 07
 Read guitar csv file
 Print a list of the give guitars sorted by year
 """
+from prac_03.capitalist_conrad import out_file
 from prac_07.guitar import Guitar
 
 
@@ -17,16 +18,63 @@ def main():
         name = parts[0]
         year = parts[1]
         cost = parts[2]
-
         guitar = Guitar(name, year, cost)
         # Add the new guitar to the list
         guitars.append(guitar)
     # Close the file after reading it
     in_file.close()
 
+    # Add a new guitar (name, year, cost)
+    print("My guitars!")
+    name = str(input("Name:")).title()
+
+    while name != "":
+        year = handle_year_exception()
+        cost = handle_cost_exception()
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{name} ({year}) : ${cost: .2f} added.")
+        name = str(input("Name:"))
+
+    # Add the new guitar to the file
+    with open("guitars.csv", "w") as out_file:
+        print(guitar), out_file
+
+
+    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+
     # Print guitars from the list (oldest to newest based on year)
-    for guitar in sorted(guitars):
-        print(guitar)
+    if guitars:  # Handle the ValueError when no guitars are given.
+        print("...snip...")
+        print("These are my guitars:")
+        for guitar in sorted(guitars):
+            print(guitar)
+    else:
+        print("There are no guitars :<, buy one!")
+
+
+def handle_cost_exception():
+    """Handle cost exceptions (non float or non integer input)."""
+    while True:
+        try:
+            cost = float(input("Cost: $"))
+            return cost
+        except ValueError:
+            print("Cost must be a number.")
+
+
+def handle_year_exception():
+    """Handle year exceptions (non integer input)."""
+    while True:
+        try:
+            year = int(input("Year:"))
+            return year
+        except ValueError:
+            print("Year must be an integer (e.g. 2020).")
+
+
 
 
 main()
