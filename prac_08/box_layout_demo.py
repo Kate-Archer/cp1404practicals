899e1dc75d6a6477fa3ef93a9c754a9e101113d8
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-11 13:12:19 +1000

    Update the box_layout demo to take in a name input and print a greeting or clear the name

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -1,5 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+status_text = StringProperty()
 
 
 class BoxLayoutDemo(App):
@@ -7,6 +10,12 @@ class BoxLayoutDemo(App):
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
+
+    def press_clear(self):
+        self.root.ids.input_name.text = ""
+
+    def handle_greet(self):
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
 BoxLayoutDemo().run()
