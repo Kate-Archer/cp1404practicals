18f89985f91b039f1e7171c8a2bf9c41c14c1990
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-11 13:16:10 +1000

    Add necessary docstrings

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -6,15 +6,19 @@ status_text = StringProperty()
 
 
 class BoxLayoutDemo(App):
+    """Boxlayout class."""
     def build(self):
+        """Build the Box Layout GUI."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def press_clear(self):
+        """Clear the inputted name."""
         self.root.ids.input_name.text = ""
 
     def handle_greet(self):
+        """Handle the greet button pressed by greeting the user (with the name input)."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
 
