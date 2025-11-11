cf64a0c7f2be164659fb1e2db3d51db584b7aa26
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-11 16:05:16 +1000

    Create a basic format for the dynamic name labels prac

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,44 @@
+"""
+Dynamically create labels based on a list of names
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
+from kivy.properties import StringProperty
+
+from prac_05.kivy_layout import Label
+
+NEW_COLOUR = (1, 0, 0, 1)  # RGBA for red
+ALTERNATIVE_COLOUR = (1, 0, 1, 1)  # RGBA for magenta
+
+
+class DynamicWidgetsApp(App):
+    """Main program - Kivy app to create dynamic widgets based on a list of names."""
+    status_text = StringProperty()
+
+    def __init__(self, name):
+        """Construct main app using name."""
+        self.name = name
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Name Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """Create labels from the given names and add them to the GUI."""
+        for name in self.name:
+            # create a name for each name entry, specifying the text
+            temp_label = Label(text=name)
+            temp_label.bind(on_press=self.press_entry)
+            # set the button's background colour
+            temp_label.background_color = NEW_COLOUR
+            # add the button to the "entries_box" layout widget
+            self.root.ids.entries_box.add_widget(temp_label)
+
+    def clear_all(self):
+        """Clear all widgets that are children of the "entries_box" layout widget."""
+        self.root.ids.entries_box.clear_widgets()
