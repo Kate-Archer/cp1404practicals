9e357accc33ce9b85364f7059c6c665e54da3d9d
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-18 13:49:29 +1000

    Add a string and get fare method for the SilverServiceTaxi class, using inherited functions from Taxi

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,22 +3,25 @@ from prac_09.taxi import Taxi
 class SilverServiceTaxi(Taxi):
     """Silver Service taxi that inherits from a Taxi class."""
 
-    Taxi.price_per_km
+    # Extra charge for each new fare
+    flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness: float):
         super().__init__(name, fuel)
         self.fanciness = fanciness
+        self.price_per_km *= fanciness
 
-    def get_price_per_km(self):
-        """Return the price for the taxi trip."""
-        return self.price_per_km * self.fanciness
+    def __str__(self):
+        """Return a string representation of a SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
 
-
-    def drive(self, distance):
-        """Drive like parent Car but calculate fare distance as well."""
-        distance_driven = super().drive(distance)
-        self.current_fare_distance += distance_driven
-        return distance_driven
+    def get_fare(self):
+        """
+        Get the current fare for the Silver service.
+        This inherits from the Taxi class using get_fare
+        and adding the flag fall for the Silver service.
+        """
+        return self.flagfall + super().get_fare()
 
 
 
