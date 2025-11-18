323b7d216612a6972f6a02860b092fcf13fa4fd9
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-18 12:29:43 +1000

    Drive the car 100km after resetting the fare

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -3,8 +3,18 @@ Test first taxi using the Taxi and Car class functions
 """
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Pruis 1", 100, 1.23)
-my_taxi.drive(40)
-print(my_taxi)
+
+def main():
+    """Test the Taxi class."""
+
+    my_taxi = Taxi("Pruis 1", 100, 1.23)
+    my_taxi.drive(40) # Drive 40km
+    print(my_taxi)
+
+    my_taxi.start_fare() # Reset fare to 0
+    print(my_taxi)
+    my_taxi.drive(100) # Drive 100km
+    print(my_taxi)
 
 
+main()
