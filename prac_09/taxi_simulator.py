4219528f710daf07f6b99e099f2edac78de053c3
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-18 15:22:36 +1000

    Begin layout for taxi simulator

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,29 @@
+"""
+Taxi simulator
+"""
+
+from prac_06.car import Car
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+# list of available taxis
+# choose how far they drive
+# show the trip cost and add to bill
+# list of taxi objects []
+current_taxi = None # Starts at zero initially
+
+def main():
+    """---"""
+    # info
+    # menu
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            pass
+
+
+main()
