1d084569d97a469c91262681086835c3e82789df
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-18 12:36:19 +1000

    Finish the Taxi test practical

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,13 +7,14 @@ from prac_09.taxi import Taxi
 def main():
     """Test the Taxi class."""
 
-    my_taxi = Taxi("Pruis 1", 100, 1.23)
-    my_taxi.drive(40) # Drive 40km
+    my_taxi = Taxi("Pruis 1", 100)
+    my_taxi.drive(40)  # Drive 40km
     print(my_taxi)
 
-    my_taxi.start_fare() # Reset fare to 0
+    my_taxi.start_fare()  # Reset fare to 0
     print(my_taxi)
-    my_taxi.drive(100) # Drive 100km
+
+    my_taxi.drive(100)  # Drive 100km
     print(my_taxi)
 
 
