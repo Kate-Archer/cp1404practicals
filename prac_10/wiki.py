3e3d84678343bd1958804e69007e2e6ba489bd84
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-25 15:48:09 +1000

    Build basic layout for exception errors

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,10 +1,22 @@
 import wikipedia
-from wikipedia import search, summary, page
+from bs4 import BeautifulSoup
+from wikipedia import search, summary, page, DisambiguationError
 
-
-
-search_input = input("Please enter a page title or search phrase:")
+search_input = input("Enter page title:")
 while search_input != "":
+    try:
+
+        search(search_input)
+        summary(search_input)
+        page(search_input, auto_suggest= False)
+
+    except NotFoundError:
+        print(f"Page id {search_input} does not match any pages. Try another id!")
+    except DisambiguationError:
+        print(f"We need a more specific title. Try one of the following, or a new search")
+        print(BeautifulSoup)
+        page(search_input)
     search(search_input)
     summary(search_input)
-    page(search_input)
+
+
