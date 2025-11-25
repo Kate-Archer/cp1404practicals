dc30bba61ef5c32caa558ce8b927c300bb731aaa
Kate Archer <kate.archer1@my.jcu.edu>
2025-11-25 16:18:01 +1000

    Complete wiki prac with exception handling

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,22 +1,26 @@
+"""
+Wikipedia prac 10
+"""
+
 import wikipedia
 from bs4 import BeautifulSoup
-from wikipedia import search, summary, page, DisambiguationError
+from wikipedia import search, summary, page, DisambiguationError, PageError
 
-search_input = input("Enter page title:")
+search_input = input("Enter page title: ")
 while search_input != "":
     try:
+        # Try to load the page normally
+        print(wikipedia.search(search_input))
+        print(wikipedia.summary(search_input))
+        print(wikipedia.page(search_input, auto_suggest=False).url)
 
-        search(search_input)
-        summary(search_input)
-        page(search_input, auto_suggest= False)
+    except PageError:
+        print(f"Page id '{search_input}' does not match any pages. Try another id!")
 
-    except NotFoundError:
-        print(f"Page id {search_input} does not match any pages. Try another id!")
-    except DisambiguationError:
-        print(f"We need a more specific title. Try one of the following, or a new search")
-        print(BeautifulSoup)
-        page(search_input)
-    search(search_input)
-    summary(search_input)
+    except DisambiguationError as error:
+        print("We need a more specific title. Try one of the following:")
+        print(error.options)  # Show list of options
 
+    search_input = input("\nEnter page title: ")
 
+print("Thank you.")
